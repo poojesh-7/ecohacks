ba04da1718508e7f2839bca2db0fe56a39b65120
poojesh-7 <poojeshks@gmail.com>
2025-06-28 19:25:24 +0530

    changes of types

--- app/[hacktype]/[hack]/page.tsx
+++ app/[hacktype]/[hack]/page.tsx
@@ -1,6 +1,5 @@
 import ViewSingleHack from "@/components/main_sections/ViewSingleHack";
 import { getSingleHack } from "@/lib/action";
-import React from "react";
 
 interface PageProps {
   params: {
@@ -11,7 +10,8 @@ interface PageProps {
 
 const SingleHackPage = async ({ params }: PageProps) => {
   const hackArr = await getSingleHack(params.hack);
-  if (!hackArr) return <div>404</div>;
+
+  if (!hackArr) return <div>hack not found</div>
 
   return <ViewSingleHack hack={hackArr[0]} similarhacks={hackArr[1]} />;
 };
